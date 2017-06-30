b58d507ef75815de3faa7779ec59e60b510e37c4
benq1211 <511648108@qq.com>
2017-06-30 17:56:09 +0800

    template

--- BLL/settings.py
+++ BLL/settings.py
@@ -71,7 +71,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'BLL.wsgi.application'
 
-
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -120,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = '/'
 admin.AdminSite.site_header = 'BLL运维系统管理后台'
--- asset/views.py
+++ asset/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, CreateView, DetailView
+from .models import Asset
 # Create your views here.
+class IndexView(ListView):
+    model = Asset
+    template_name = 'asset/index.html'
+    context_object_name = 'asset_list'
+    fields = ['ip', 'hostname', 'group','env','cpu','memory','disk']
+
+
+
