3e2cca990d1c195611cfb181ac2259dcdcf3906c
benq <511648108@qq.com>
2017-07-12 21:34:50 +0800

    add 搜索

--- asset/urls.py
+++ asset/urls.py
@@ -3,5 +3,5 @@ from . import views
 app_name = 'asset'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^add/$',views.AssetCreateView.as_view(),name='add'),
+    url(r'^add/$', views.AssetCreateView.as_view(), name='add'),
 ]
