2639f74965d173ebb40e827691591ba5c45def0c
benq <a511648108>
2017-11-17 15:49:48 +0800

    删除重复的url连接

--- asset/urls_BASE_5376.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-app_name = 'asset'
-urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^add/$',views.AssetCreateView.as_view(),name='add'),
-]
